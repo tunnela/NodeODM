3d0cdc6ed8959eb4fff8b814d874786ab603f070
Lauri Tunnela <lauri@paranotek.com>
2022-11-23 13:03:52 +0200

    Ask whether to publish the new version

--- docker/build.bat
+++ docker/build.bat
@@ -4,8 +4,8 @@ setlocal enableDelayedExpansion
 set /P version="Give version number for the new docker image release: "
 
 if not defined version (
-	call npx echo-cli "Please give a version number."
-	exit
+    call npx echo-cli "Please give a version number."
+    exit
 )
 
 cd ..
@@ -14,5 +14,12 @@ call docker build -f ./Dockerfile -t tunnela/nodeodm:!version! -t tunnela/nodeod
 
 cd ./docker
 
+set /P publish="Would you like to publish the new docker image? [y|n]: "
+
+if not !publish! equ yes if not !publish! equ y (
+    exit
+)
+
+call docker login
 call docker push tunnela/nodeodm:!version!
 call docker push tunnela/nodeodm:latest
--- docker/build.sh
+++ docker/build.sh
@@ -1,12 +1,10 @@
-read -p "Give version number for the new docker image release: " version
+read -p "Give version number for the new docker image: " version
 
 if [ "$version" = "" ]
 then
-	echo "Please give a version number!"
-	exit
+    echo "Please give a version number!"
+    exit
 fi
-
-docker login
 
 cd ..
 
@@ -14,5 +12,13 @@ docker build -f ./Dockerfile -t tunnela/nodeodm:$version -t tunnela/nodeodm:late
 
 cd ./docker
 
+read -p "Would you like to publish the new docker image? [y|n]: " publish
+
+if [[ "$publish" != "y" && "$matches" != "yes" ]]
+then
+    exit
+fi
+
+docker login
 docker push tunnela/nodeodm:$version
 docker push tunnela/nodeodm:latest
